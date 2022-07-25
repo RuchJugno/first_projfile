2c791097c59fee5c021b731051bcd253f671287c
RuchJugno <109935531+RuchJugno@users.noreply.github.com>
2022-07-25 12:15:25 +0800

    Update streamlit_app.py
    fixed type

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 Blueberry Outmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamit.text('Bard-Boiled Free-Range Egg')
+streamlit.text('Bard-Boiled Free-Range Egg')
 
