18cd52c63739ba95e018348ffac3a286c6dbdc3a
RuchJugno <109935531+RuchJugno@users.noreply.github.com>
2022-07-25 12:14:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner Part2')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 Blueberry Outmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Bard-Boiled Free-Range Egg')
+
