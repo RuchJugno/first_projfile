5e81669b7232b8cf9962f68a96c015218c477827
RuchJugno <109935531+RuchJugno@users.noreply.github.com>
2022-08-02 19:58:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,10 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
+
+
+#New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice2 = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice2)
+
